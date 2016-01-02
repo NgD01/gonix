cac45d6973bb01e545580a39242fa2ab7eb230f0
Dominic Hamon <dma+github@stripysock.com>
2016-01-01 23:29:52 -0800

    running 64-bit code!

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -13,9 +14,16 @@ start:
   call setup_page_tables
   call enable_paging
 
-  ; print 'OK' to screen
-  mov dword [0xb8000], 0x2f4b2f4f
-  hlt
+  ; load 64-bit GDT
+  lgdt [gdt64.pointer]
+
+  ; update selectors
+  mov ax, gdt64.data
+  mov ss, ax
+  mov ds, ax
+  mov es, ax
+
+  jmp gdt64.code:long_mode_start
 
 ; create page tables and ensure entries are mapped
 setup_page_tables:
@@ -132,4 +140,16 @@ p2_table:
 stack_bottom:
   resb 64
 stack_top:
+
+
+section .rodata
+gdt64:
+  dq 0
+.code: equ $ - gdt64
+  dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53)  ; code segment
+.data: equ $ - gdt64
+  dq (1<<44) | (1<<47) | (1<<41)  ; data segment
+.pointer:
+  dw $ - gdt64 - 1
+  dq gdt64
 
